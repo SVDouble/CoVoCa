596cbe6161304b1d474d1b0f7b0604f854f9ef1b
Tingwei Feng <tingwei.feng@rwth-aachen.de>
2026-06-29 16:09:04 +0200

    Color Info

--- include/Voxel.h
+++ include/Voxel.h
@@ -14,12 +14,15 @@ public:
     Eigen::Vector3d getCartesianPos() const;
     Eigen::Vector3i getIndexPos() const;
     bool getOccupied() const;
+    Eigen::Vector3i getColor() const;
 
     // Setters
     void setOccupied(bool _new_occupied);
+    void setColor(const Eigen::Vector3i &_color);
 
 private:
     Eigen::Vector3d m_cartesian_pos; // Cartesian position of voxel center
     Eigen::Vector3i m_index_pos;     // Indices in voxel grid [row, column, depth]
     bool m_occupied;
+    Eigen::Vector3i m_color;         // RGB color [0, 255]
 };
--- src/Voxel.cpp
+++ src/Voxel.cpp
@@ -5,6 +5,7 @@ Voxel::Voxel()
     m_cartesian_pos = Eigen::Vector3d(0.0, 0.0, 0.0);
     m_index_pos = Eigen::Vector3i(-1, -1, -1);
     m_occupied = true;
+    m_color = Eigen::Vector3i(128, 128, 128);
 }
 
 Voxel::Voxel(Eigen::Vector3d _cartesian_pos, Eigen::Vector3i _index_pos)
@@ -12,6 +13,7 @@ Voxel::Voxel(Eigen::Vector3d _cartesian_pos, Eigen::Vector3i _index_pos)
     m_cartesian_pos = _cartesian_pos;
     m_index_pos = _index_pos;
     m_occupied = true;
+    m_color = Eigen::Vector3i(128, 128, 128);
 }
 
 Voxel::Voxel(Eigen::Vector3d _cartesian_pos, Eigen::Vector3i _index_pos, bool _occupied)
@@ -19,6 +21,7 @@ Voxel::Voxel(Eigen::Vector3d _cartesian_pos, Eigen::Vector3i _index_pos, bool _o
     m_cartesian_pos = _cartesian_pos;
     m_index_pos = _index_pos;
     m_occupied = _occupied;
+    m_color = Eigen::Vector3i(128, 128, 128);
 }
 
 Eigen::Vector3d Voxel::getCartesianPos() const
@@ -39,4 +42,14 @@ bool Voxel::getOccupied() const
 void Voxel::setOccupied(bool _new_occupied)
 {
     m_occupied = _new_occupied;
+}
+
+Eigen::Vector3i Voxel::getColor() const
+{
+    return m_color;
+}
+
+void Voxel::setColor(const Eigen::Vector3i &_color)
+{
+    m_color = _color;
 }
