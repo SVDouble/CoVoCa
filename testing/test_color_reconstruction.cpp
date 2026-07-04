c3d84c8b34de1ef94d74f4747ef3f30d7371b519
Maracondaa <anna.len.mueller@gmail.com>
2026-07-04 16:05:06 +0200

    changed test so thresholding can be used for silhouette extraction

--- testing/test_color_reconstruction.cpp
+++ testing/test_color_reconstruction.cpp
@@ -1,14 +1,15 @@
-#include "ColorReconstructor.h"
-#include "VoxelGrid.h"
-#include "VoxelCarver.h"
-#include "View.h"
 #include "Camera.h"
+#include "ColorReconstructor.h"
 #include "HelperFunctions.h"
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <vector>
-#include <string>
+#include "ThresholdExtractor.h"
+#include "View.h"
+#include "VoxelCarver.h"
+#include "VoxelGrid.h"
 #include <filesystem>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
 
 namespace fs = std::filesystem;
 
@@ -67,18 +68,32 @@ int main(int argc, char** argv) {
   std::cout << "Voxelgrid size: " << voxel_grid.getSize() << std::endl;
   std::cout << "Voxelgrid stepsize: " << voxel_grid.getStepSize() << std::endl;
 
-  std::string folder_images = "../dataset/dino/images";
+  std::string folder_images = "../dataset/dinoSparseRing/images";
   std::vector<cv::Mat> imageVector = loadImages(folder_images);
 
-  std::string folder_masks = "../dataset/dino/masks";
-  std::vector<cv::Mat> silhouetteVector = loadImages(folder_masks);
+  // Extract silhouette (if needed)
 
-  std::string camera_file = "../dataset/dino/images/dino_par.txt";
+  ThresholdConfig thresholdConfig;
+  thresholdConfig.diffThreshold = 60;
+  ThresholdExtractor silhouette_extractor = ThresholdExtractor(thresholdConfig);
+
+  for (size_t i = 0; i < imageVector.size(); ++i)
+  {
+    cv::Mat silhouette = silhouette_extractor.extract(imageVector[i]);
+    silhouette_vector.push_back(silhouette);
+    //std::string filename = "silhouette_" + std::to_string(i) + ".png";
+    //silhouette_extractor.saveSilhouette(silhouette, filename);
+  }
+
+  //std::string folder_masks = "../dataset/dinoSparseRing/masks";
+  //std::vector<cv::Mat> silhouette_vector = loadImages(folder_masks);
+
+  std::string camera_file = "../dataset/dinoSparseRing/dinoSR_par.txt";
   camera_vector = loadCameras(camera_file);
 
   std::vector<View> views;
 
-  if (imageVector.size() != silhouetteVector.size() ||
+  if (imageVector.size() != silhouette_vector.size() ||
       imageVector.size() != camera_vector.size()) {
     std::cerr << "Error: Image, silhouette, and camera counts do not match!\n";
     return -1;
@@ -87,7 +102,7 @@ int main(int argc, char** argv) {
   for (size_t i = 0; i < imageVector.size(); ++i) {
     View view;
     view.colorImage = imageVector[i];
-    view.silhouette = silhouetteVector[i];
+    view.silhouette = silhouette_vector[i];
     view.camera = camera_vector[i];
     views.push_back(view);
   }
